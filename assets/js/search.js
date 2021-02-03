7f0b005478c80ba3617c588246b4a32f014ed36c
webuiprovider <webuiprovider@gmail.com>
2021-02-03 18:35:37 +0530

    updated search

--- assets/js/search.js
+++ assets/js/search.js
@@ -7,7 +7,7 @@
 
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
-        appendString += '<li class="col-lg-4 col-md-4 blogBox"><div class="item"><img src="' + item.image + '"><a href="' + item.url + '"><h3>' + item.title +'</h3></a>';      
+        appendString += '<li class="col-lg-4 col-md-4 blogBox"><div class="item"><img src="' + item.image + '"><div class="publice"><a href="' + item.url + '"><h3>' + item.title +'</h3></a></div>';      
        // appendString += '<p>' + item.content.substring(0, 150) + '...</p></div></li>';
       }
 
